a4aba7fd750adbc2e1344a2dee7ed5ddaadbdda3
wellingtonntonn@gmail.com <wellingtonntonn@gmail.com>
2020-07-10 14:29:24 -0300

    ok

--- Frontend/react-login-video-1-final/src/components/User/Register/Register.jsx
+++ Frontend/react-login-video-1-final/src/components/User/Register/Register.jsx
@@ -29,8 +29,8 @@ const Register = () => {
     });
   }
 
-  async function register({ email, password }) {
-    const { status, data } = await service.register(email, password);
+  async function register() {
+    const { status, data } = await service.register(values.email, values.password);
     if (status === 200) {
       return { token: data.token };
     }
@@ -40,7 +40,7 @@ const Register = () => {
   async function onSubmit(event) {
     event.preventDefault();
 
-    const { token, error } = await register(values);
+    const { token, error } = await register();
 
     if (token) {
       setToken(token);
@@ -65,6 +65,7 @@ const Register = () => {
             onChange={onChange}
             value={values.email}
             placeholder="E-mail"
+            required
           />
         </div>
         <div className="user-login__form-control">
@@ -75,7 +76,8 @@ const Register = () => {
             name="password"
             onChange={onChange}
             value={values.password}
-            placeholder=" Senha "
+            placeholder="Senha"
+            required
           />
         </div>
         {error && (
